a587dfc893e238a027572de2d21fe40d5703f595
ribeiro-dev <romilson.2r@gmail.com>
2023-02-25 13:28:49 -0300

    get weather by location and change page style

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-var x = document.getElementById("container");
+var x = document.getElementById("content");
+const temperatureElement = document.querySelector('#temperature')
+const feelsLikeElement = document.querySelector('#feels-like')
 
 const key = config.API_KEY
 
@@ -11,19 +13,27 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    const coords = position.coords
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+  const coords = position.coords
+  // x.innerHTML = "Latitude: " + position.coords.latitude +
+  // "<br>Longitude: " + position.coords.longitude;
 
-    console.log(coords)
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&lang=pt_br&units=metric&appid=${key}`
+  console.log(coords)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&lang=pt_br&units=metric&appid=${key}`
 
-    fetch(url) // faz um get na API
-        .then(res => {
-            return res.json() // converte a response em json
-        })
-        .then(res => {
-            console.log(res) 
-        })
+  fetch(url) // faz um get na API
+    .then(res => {
+      return res.json() // converte a response em json
+    })
+    .then(res => {
+      // x.innerHTML += res.weather[0].description
+      console.log(res) 
+      console.log(res.main)
+      let temperature = res.main.temp
+      let feelsLike = res.main.feels_like
+      // console.log(typeof(temperature))
+      
+      temperatureElement.textContent = parseInt(temperature) + "º"
+      feelsLikeElement.textContent = `Sensação térmica de ${parseInt(feelsLike)}º`
+    })
 }
 getLocation()
